dd92335c966bccd12be14b1fcf34cc4455a7bb67
Canelli331 <sebastiano.canelli@studenti.iispascal.it>
2026-03-20 10:18:20 +0100

    classe gatto

---
+++ prgAnimali/src/prganimali/Gatto.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prganimali;
+
+/**
+ *
+ * @author canellis
+ */
+public class Gatto extends Animale {
+    
+    private String razza;
+
+    public Gatto(String nome, int eta,String razza) {
+        super(nome, eta);
+        this.razza = razza;
+    }
+
+    public String getRazza() {
+        return razza;
+    }
+    
+    @Override
+    public String verso(){
+        return "miao";
+    }
+    
+    @Override
+    public String descrizione(){
+        return "Gatto: " + getNome() + ", eta': " + getEta() + ", razza: "+ razza + ", verso: " + verso();
+    }
+    
+    public void faiLeFusa(){
+        System.out.println(getNome() + " sta facendo le fusa");
+    }
+
+    
+}
